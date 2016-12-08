7a266c884ffb71834c19d329a387d7004e7db15b
Chana Reynolds <chana777@gmail.com>
2016-12-08 16:34:11 -0700

    Added-More-Shows

--- seeds/show.js
+++ seeds/show.js
@@ -6,7 +6,22 @@ exports.seed = function(knex, Promise) {
       const shows = [{
          date: '1/2/2055',
          location: 'Platte St',
-       }];
+         time: "5pm",
+         designer: "Gucci",
+         model: "Gigi",
+      },{
+         date: '5/6/20',
+         location: 'Poop Rd',
+         time: "10pm",
+         designer: "Balmain",
+         model: "Bella",
+      },{
+         date: '3/4/2065',
+         location: 'Sleep Please St',
+         time: "9pm",
+         designer: "Valentino",
+         model: "Lara Stone",
+      }];
        return knex('show').insert(shows);
     });
 };
